0cd8841077db59bef9e60b0e1bc123d72e63f625
kstbhdr <kstbhdr@gmail.com>
2026-05-09 13:00:23 +0300

    chore: bump version to 1.0.3

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const DEHA_VERSION = '1.0.2';
-export const DEHA_SEMVER = '1.0.2';
+export const DEHA_VERSION = '1.0.3';
+export const DEHA_SEMVER = '1.0.3';
 export const DEHA_VERSION_LABEL = `DEHA CLI Version ${DEHA_VERSION}`;
